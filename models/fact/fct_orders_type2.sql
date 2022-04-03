4f34592df9c01c2091d80ccb595d0e53cea3d556
Maxwell Lee <maxwelllee@Maxwells-MacBook-Air.local>
2022-04-03 16:46:32 -0400

    update products seed

--- models/fact/fct_orders_type2.sql
+++ models/fact/fct_orders_type2.sql
@@ -9,6 +9,6 @@ FROM
     LEFT JOIN {{ ref('dim_products_scd2') }} p
         ON o.product_id = p.product_id
         AND o.date_created >= p.date_effective
-        AND o.date_created > p.date_expire
+        AND o.date_created < p.date_expire
     LEFT JOIN {{ ref('dim_customers') }} c
         ON o.customer_id = c.customer_id
