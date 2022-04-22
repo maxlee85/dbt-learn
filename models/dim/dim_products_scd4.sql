2b800611ddde3f466dda45d34e187a12e543bd17
maxlee85 <maxwellclee@gmail.com>
2022-04-22 17:34:09 -0400

    add type 4 scd

---
+++ models/dim/dim_products_scd4.sql
@@ -0,0 +1,22 @@
+/*
+Type 4 = rapidly changing dimensions. Create a table containing all possible
+combinations of the dimension. For example, if the color of the products changed
+every day than color would be added to a "mini dimension" table.
+
+So fct_orders would contain a fk to dim_products and dim_product_colors.
+*/
+WITH products AS (
+    SELECT DISTINCT
+      product_id,
+      product_name,
+      size
+    FROM
+        {{ ref('products') }}
+)
+SELECT
+    ROW_NUMBER() OVER (ORDER BY product_id) AS product_key,
+    product_id,
+    product_name,
+    size
+FROM
+    products
