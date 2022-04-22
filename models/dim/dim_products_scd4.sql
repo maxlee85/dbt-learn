5fd2dea2454fd6be4ce5c1575b12755de841a05b
maxlee85 <maxwellclee@gmail.com>
2022-04-22 17:02:49 -0400

    update primary key

--- models/dim/dim_products_scd4.sql
+++ models/dim/dim_products_scd4.sql
@@ -6,11 +6,11 @@ every day than color would be added to a "mini dimension" table.
 So fct_orders would contain a fk to dim_products and dim_product_colors.
 */
 SELECT 1 AS product_color_key, 'grey' AS color UNION ALL
-SELECT 1 AS product_color_key, 'black' AS color UNION ALL
-SELECT 1 AS product_color_key, 'green' AS color UNION ALL
-SELECT 1 AS product_color_key, 'blue' AS color UNION ALL
-SELECT 1 AS product_color_key, 'yellow' AS color UNION ALL
-SELECT 1 AS product_color_key, 'white' AS color UNION ALL
-SELECT 1 AS product_color_key, 'red' AS color UNION ALL
-SELECT 1 AS product_color_key, 'purple' AS color UNION ALL
-SELECT 1 AS product_color_key, 'brown' AS color
+SELECT 2 AS product_color_key, 'black' AS color UNION ALL
+SELECT 3 AS product_color_key, 'green' AS color UNION ALL
+SELECT 4 AS product_color_key, 'blue' AS color UNION ALL
+SELECT 5 AS product_color_key, 'yellow' AS color UNION ALL
+SELECT 6 AS product_color_key, 'white' AS color UNION ALL
+SELECT 7 AS product_color_key, 'red' AS color UNION ALL
+SELECT 8 AS product_color_key, 'purple' AS color UNION ALL
+SELECT 9 AS product_color_key, 'brown' AS color
